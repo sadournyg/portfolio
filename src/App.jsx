a0ca8f612c6b7ff8033fa479d8678c5dae70aa60
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 13:11:00 +0100

    feat: add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./assets/components/Navbar";
 
 const App = () => {
-  return <div className="text-2xl text-red-300">App</div>;
+  return (
+    <div className="text-2xl text-red-300">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
