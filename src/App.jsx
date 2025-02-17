93c5efcc313fc4e195db8f4cfdab06b2d17b1db7
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 16:04:38 +0100

    feat: add Portfolio component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
+import Portfolio from "./components/Portfolio";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Skills />
+      <Portfolio />
     </div>
   );
 };
