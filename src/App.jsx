ece277f2235407e37977d7a07636ca46ab5aa783
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 16:15:37 +0100

    feat: add Experience component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Skills />
       <Portfolio />
+      <Experience />
     </div>
   );
 };
