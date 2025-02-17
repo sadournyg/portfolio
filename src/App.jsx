dc9cb0d14c3646e10826dee9831421928d19894d
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 14:56:03 +0100

    feat: add Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Skills from "./components/Skills";
 
 const App = () => {
   return (
     <div className="text-2xl text-red-300">
       <Navbar />
       <Hero />
+      <Skills />
     </div>
   );
 };
