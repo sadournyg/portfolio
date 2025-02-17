67fedb0d405c5e9939b9fbae23a916ede9e7345a
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 16:48:37 +0100

    feat: completed contact component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Skills />
       <Portfolio />
       <Experience />
+      <Contact />
     </div>
   );
 };
