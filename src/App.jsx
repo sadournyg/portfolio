63dc35311c27fdad507183784cf87296169bf0da
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-02-17 16:55:15 +0100

    feat : completed Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Skills from "./components/Skills";
 import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Portfolio />
       <Experience />
       <Contact />
+      <Footer />
     </div>
   );
 };
