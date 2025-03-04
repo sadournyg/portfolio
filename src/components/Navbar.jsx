71156e500bd701c9278e58f5ba955d4fe10c8e0e
Guillaume Sadourny <guillaume.sadourny@ecoles-epsi.net>
2025-03-04 15:12:45 +0100

    fix: Fixing responsive navbar , working good after fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,8 +31,8 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50 overflow-x-hidden">
-      <div className="max-w-[1300px] mx-auto  flex justify-between text-gray-100 text-xl items-center px-4 md:px-12 h-20">
+    <div className="fixed top-0 left-0 w-full bg-opacity-70 backdrop-blur-md z-50">
+      <div className="max-w-[1300px] mx-auto flex justify-between text-gray-100 text-xl items-center px-4 md:px-12 h-20">
         <a href="#">J.Doe</a>
         <ul className="hidden md:flex gap-12 z-10 cursor-pointer">
           <li>
@@ -51,52 +51,53 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
-        <div onClick={toggleNav} className="md:hidden z-50 text-gray-200">
+        <div onClick={toggleNav} className="md:hidden z-70 text-gray-200">
           {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
         </div>
-        <motion.div
-          initial={false}
-          animate={nav ? "open" : "closed"}
-          variants={menuVariants}
-          className="fixed left-0 top-0 w-screen min-h-screen bg-gray-900 z-40"
-        >
-          <ul className="font-semibold text-4xl space-y-8 mt-24 text-center cursor-pointer ">
-            <li>
-              <Link
-                to="skills"
-                onClick={closeNav}
-                smooth={true}
-                offset={50}
-                duration={500}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="portfolio"
-                onClick={closeNav}
-                smooth={true}
-                offset={50}
-                duration={500}
-              >
-                Portfolio
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="contact"
-                onClick={closeNav}
-                smooth={true}
-                offset={50}
-                duration={500}
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </motion.div>
       </div>
+
+      <motion.div
+        initial={false}
+        animate={nav ? "open" : "closed"}
+        variants={menuVariants}
+        className="fixed inset-0 w-screen h-screen bg-gray-900 text-gray-200 z-50 flex flex-col items-center justify-center"
+      >
+        <ul className="font-semibold text-4xl space-y-8 text-center cursor-pointer">
+          <li>
+            <Link
+              to="skills"
+              onClick={closeNav}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="portfolio"
+              onClick={closeNav}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="contact"
+              onClick={closeNav}
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </motion.div>
     </div>
   );
 };
